1dd3176ee708a29b9459d5bdc7a1184137fb1aa4
Kisho Linux <kisokumar886@gmail.com>
2022-01-08 20:15:21 +0000

    initial version

---
+++ Password_Generator.py
@@ -0,0 +1,64 @@
+import secrets
+import string 
+import time
+import sys
+import os
+
+alphabet = string.ascii_letters + string.digits + string.punctuation #Makes an 'alphabet' containing all the ascii numbers, letters and symbols.
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear') 
+
+def typer(string,timer=(0.01),remove="no"):
+    for c in string:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(timer)
+    if remove=="yes":
+        cls()
+
+def passwordgenerator():
+    cls()
+    many = int(input(f'''
+    -----------------------------------------------------------------
+    --------------------------Password Generator---------------------
+    -----------------------------------------------------------------
+    
+    How many passwords would you like? : '''))
+    time.sleep(0.5)
+    cls()
+    rand = int(input(f'''
+    -----------------------------------------------------------------
+    --------------------------Password Generator---------------------
+    -----------------------------------------------------------------
+
+    How many characters should the passwords be? : '''))
+    cls()
+    print('\n\nWorking . . .')
+    time.sleep(1)
+    cls()
+    alphabet = string.ascii_letters + string.digits + string.punctuation #Makes an 'alphabet' containing all the ascii numbers, letters and symbols.
+    y=0
+    for x in range(many): #Amount of passwords
+        y+=1
+        password = "".join(secrets.choice(alphabet) for i in range(rand)) #Joins a random assorted string made up of the 'alphabet' made, for the amount of letters specified
+        temp = f'''Password # {y} - - -> {password}\n'''
+        typer(temp)
+        time.sleep(0.1)
+    #region
+    print("\n\n\n\nDone!\n\nYou have 60 seconds to save passwords with either a screenshot or the clipboard! \n\n\n\n")
+    time.sleep(60)
+    cls()
+    print("I will now exit automatically!")
+    for i in range(3):
+        time.sleep(1)
+        print(f"{i} !")
+        cls()
+    print("Goodbye!")
+    time.sleep(3)
+    cls()
+    exit()
+    #endregion
+
+if __name__=="__main__":
+    passwordgenerator()
